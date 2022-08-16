afece72a03cb0adc8cd3dc716733b444af197a9d
cansoy <cansoymuhammed@hotmail.com>
2022-08-16 07:12:40 +0300

    make it better

--- app3.js
+++ app3.js
@@ -1,40 +1,41 @@
-// Single page application
+// multi page application
 const puppeter=require('puppeteer')
 const cheerio =require('cheerio')
 const fs = require('fs')
 
 const scrape=async()=>{
+    const mainUrl='https://books.toscrape.com/catalogue'
+    const listOfUrl=[]
+    for (let index = 1; index < 51; index++) {
+        const newPageUrl=`${mainUrl}/page-${index}.html`
+        listOfUrl.push(newPageUrl)
+    }
+    const outOfTitlesAndPrices=[]
     const browser=await puppeter.launch({headless:false,devtools:false})
     const page=await browser.newPage()
-    await page.goto('https://books.toscrape.com/catalogue/page-1.html',{waitUntil:'networkidle2'})
-    await page.waitForSelector('h1')
-    const titlesAndPrices=await page.$$eval('ol[class=row] li[class]',elems=>{
-        return elems.map(item=>{
-            return{
-                title:item.querySelector('h3>a').textContent,
-                price:item.querySelector('div[class=product_price] p[class=price_color]').textContent
-            }
-        })
-    })
-    console.log(titlesAndPrices)
-
-    // const page_2=await browser.newPage()
-    // await page_2.goto('https://books.toscrape.com/catalogue/page-2.html',{waitUntil:'networkidle2'})
-    // await page_2.waitForSelector('h1')
-    // const titlesAndPrices_2=await page_2.$$eval('ol[class=row] li[class]',elems=>{
-    //     return elems.map(item=>{
-    //         return {
-    //             title:item.querySelector('h3>a').textContent,
-    //             price:item.querySelector('div[class=product_price] p[class=price_color]').textContent
-    //         }
-    //     })
-    // })
-    // console.log(titlesAndPrices_2)
-
-
-
-
-
+    try {
+        for (let index = 0; index < listOfUrl.length; index++) {
+            await page.goto(listOfUrl[index],{waitUntil:'networkidle2'})
+            await page.waitForSelector('h1')
+            const titlesAndPrices=await page.$$eval('ol[class=row] li[class]',elems=>{
+                return elems.map(item=>{
+                    return{
+                        title:item.querySelector('h3>a').textContent,
+                        price:item.querySelector('div[class=product_price] p[class=price_color]').textContent
+                    }
+                })
+            })
+            titlesAndPrices.forEach(item=>{
+                outOfTitlesAndPrices.push(item)
+            })
+        }
+        await browser.close()
+    } 
+    catch (error) {
+        console.log(error.originalMessage)
+        await browser.close()
+    }
+    console.log(outOfTitlesAndPrices)
 }
 
-// scrape()
+scrape()
